66e5e2be0326270bfca8ed53ea2cf2997c1960f5
Gaurav Tripathi <ramantripathi22@gmail.com>
2024-08-06 00:19:55 +0530

    profile update

--- resources/views/myaccount.blade.php
+++ resources/views/myaccount.blade.php
@@ -1,8 +1,9 @@
 @extends('app')
 @section('content')
-        <div class="dashboard__elements">
-        <div class="container dashboard__elements--inner">
-            My Account Contents
+<div class="dashboard__elements">
+    <div class="container dashboard__elements--inner form-container-pop">
+        <div id="edit-profile" class="edit-profile">
+            
         </div>
-        </div>
-@endsection
+    </div>
+    @endsection
