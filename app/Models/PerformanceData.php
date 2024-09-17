049cd12e58b1dfc83c1bc6dff4d44d428a865d1f
Abhishek <is.abhishek@gmail.com>
2024-09-17 22:35:16 +0530

    profile changes

---
+++ app/Models/PerformanceData.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use App\Models\TrainingSession;
+
+class PerformanceData extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+   
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    
+     protected $table = "performance_data";
+
+     protected $fillable = [
+        'user_id',
+        'performance',
+        'status',
+        'kpi_score',
+        'parameter',
+        'session_id',
+        'unit_measurement',
+        'description',
+    ];
+
+    public  function session(){
+        return $this->hasOne(TrainingSession::class,'session_id','session_id');
+    }
+}
